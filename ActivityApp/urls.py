46dffa8b34e1877c00893a18e9e78fd63315e9c0
akhil3173 <akhilkacham3173@gmail.com>
2025-04-11 12:16:28 +0530

    Add files via upload

---
+++ ActivityApp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("index.html", views.index, name="index"),
+    path("AdminLogin.html", views.AdminLogin, name="AdminLogin"),	      
+    path("AdminLoginAction", views.AdminLoginAction, name="AdminLoginAction"),
+    path("TrainCNN", views.TrainCNN, name="TrainCNN"),
+    path("DetectActivity.html", views.DetectActivity, name="DetectActivity"),
+    path("DetectActivityAction", views.DetectActivityAction, name="DetectActivityAction"),	      
+]
